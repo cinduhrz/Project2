e8b21bb254965e36dd8b725c300900dbb5d8e181
Cindy Wong <awesome7852@gmail.com>
2022-12-02 18:10:19 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+///////////////////////////////////
+// Import Dependencies
+///////////////////////////////////
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+
+
+///////////////////////////////////
+// Create Express App Object
+///////////////////////////////////
+const app = express()
+
+
+///////////////////////////////////
+// Connect to Mongo
+///////////////////////////////////
+mongoose.connect(process.env.DATABASE_URL)
+
+// Mongoose connection events
+mongoose.connection
+.on("open", () => console.log("Connected to Mongo"))
+.on("close", () => console.log("Disconnected From Mongo"))
+.on("error", (error) => console.log(error))
+
+
+///////////////////////////////////
+// Register Middleware
+///////////////////////////////////
+app.use(morgan("dev"))
+app.use("/static", express.static("public"))
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride("_method"))
+
+
+// Routers and Routes
+app.get('/', (req, res) => {
+    res.send('<h1>Server is working</h1>')
+})
+
+
+///////////////////////////////////
+// Turn Server On
+///////////////////////////////////
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+})
