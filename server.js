2cdac4b683776a68bff6dbc0f72ba503664628a2
Cindy Wong <awesome7852@gmail.com>
2022-12-10 12:38:31 -0500

    connected user router to server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const express = require('express')
 const morgan = require('morgan')
 const methodOverride = require('method-override')
 const mongoose = require('./models/connection') // connect to db
-const SessionNoteRouter = require('./controllers/sessionNote') // import sessionNote router
+const SessionNoteRouter = require('./controllers/sessionNote') // import SessionNote router
+const UserRouter = require('./controllers/user.js') // import User router
 
 
 ///////////////////////////////////
@@ -23,6 +24,7 @@ app.use("/static", express.static("public"))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride("_method"))
 // !! put user router here (has to go BEFORE other routers bc other routers will redirect to user router's login route when ppl try to access their routes without authorization)
+app.use(UserRouter)
 app.use(SessionNoteRouter)
 
 
