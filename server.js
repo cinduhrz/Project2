702a2b93d3a83a46808d94a84ff152da19721ee0
Cindy Wong <awesome7852@gmail.com>
2022-12-10 15:37:45 -0500

    started authorization

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const methodOverride = require('method-override')
 const mongoose = require('./models/connection') // connect to db
 const SessionNoteRouter = require('./controllers/sessionNote') // import SessionNote router
 const UserRouter = require('./controllers/user.js') // import User router
+const session = require('express-session') // middleware for creating session cookies
+const MongoStore = require('connect-mongo') // plugin that allows express-session to save session data in our mongo database
 
 
 ///////////////////////////////////
@@ -19,10 +21,20 @@ const app = express()
 ///////////////////////////////////
 // Register Middleware
 ///////////////////////////////////
+// Order for Authentication and Authorization
+// 1. express-session middleware (uses secret and MongoStore)
+// 2. UserRouter (must go before other routers)
+// 3. other routers
 app.use(morgan("dev"))
 app.use("/static", express.static("public"))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride("_method"))
+app.use(session({
+    secret: process.env.SECRET,
+    store: MongoStore.create({mongoUrl: process.env.DATABASE_URL}),
+    saveUninitialized: true,
+    resave: false
+}))
 // !! put user router here (has to go BEFORE other routers bc other routers will redirect to user router's login route when ppl try to access their routes without authorization)
 app.use(UserRouter)
 app.use(SessionNoteRouter)
@@ -30,6 +42,11 @@ app.use(SessionNoteRouter)
 
 // Home Route
 app.get('/', (req, res) => {
+    // if(user.loggedIn) {
+    //     res.redirect('/home')
+    // } else {
+    //     res.render('index.ejs')
+    // }
     res.render('index.ejs')
 })
 
